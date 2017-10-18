1741f99afce444c5a8837077dbc414cc59f64604
CyrilAllexandre <cyril.allexandre@gmail.com>
2017-10-18 14:49:17 +0200

    MainActivity Button and library intent
    The navigation from the main activity to the library is now working but the image selected is not transferred to the imageview of the mainActivity

--- app/src/main/java/com/example/myouf/logoreco/MainActivity.java
+++ app/src/main/java/com/example/myouf/logoreco/MainActivity.java
@@ -1,13 +1,66 @@
 package com.example.myouf.logoreco;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button captureButton;
+    Button libraryButton;
+    Button analysisButton;
+    ImageView imageView;
+
+    //Declaration des constantes code de retour des requetes intent
+    private static final int PHOTO_LIB_REQUEST = 1;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        captureButton = (Button) findViewById(R.id.captureButton);
+        captureButton.setOnClickListener(this);
+
+        libraryButton = (Button) findViewById(R.id.libraryButton);
+        libraryButton.setOnClickListener(this);
+
+        analysisButton = (Button)findViewById(R.id.analysisButton);
+        analysisButton.setOnClickListener(this);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+
     }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+
+            case R.id.captureButton:
+                //startCaptureActivity();
+                break;
+
+            case R.id.libraryButton:
+                startPhotoLibraryActivity();
+                break;
+
+            case R.id.analysisButton:
+                //startAnalysisActivity();
+                break;
+        }
+
+    }
+
+    protected void startPhotoLibraryActivity(){
+        Intent photoLibIntent = new Intent();
+        photoLibIntent.setType("image/*");
+        photoLibIntent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(photoLibIntent,PHOTO_LIB_REQUEST);
+    }
+    
 }
