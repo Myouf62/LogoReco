92420ba48fe138e9ffcb452c92d279ce5b88a7b4
CyrilAllexandre <cyril.allexandre@gmail.com>
2017-10-19 16:27:57 +0200

    Refactoring + better quality of capture

--- app/src/main/java/com/example/myouf/logoreco/MainActivity.java
+++ app/src/main/java/com/example/myouf/logoreco/MainActivity.java
@@ -1,21 +1,33 @@
 package com.example.myouf.logoreco;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import static android.R.attr.data;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -30,6 +42,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     //Declaration des constantes code de retour des requetes intent
     private static final int PHOTO_LIB_REQUEST = 1;
     private static final int CAMERA_REQUEST = 2;
+    public static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 123;
 
 
     @Override
@@ -79,15 +92,101 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     protected void onActivityResult(int requestCode, int resultCode, Intent intent){
         if (requestCode==PHOTO_LIB_REQUEST && resultCode==RESULT_OK){
-            Uri photoUri = intent.getData();
-            imageView.setImageURI(photoUri);
+            Uri selectedImageUri = intent.getData();
+            setImageViewContent(selectedImageUri);
         }
 
         if (requestCode==CAMERA_REQUEST && resultCode==RESULT_OK){
-            Bundle extras = intent.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imageView.setImageBitmap(imageBitmap);
+            Uri selectedImageUri = intent.getData();
+            setImageViewContent(selectedImageUri);
         }
     }
-    
+
+    /**
+     * This function set the imageView with the selected picture
+     * @param selectedImageUri
+     */
+    public void setImageViewContent(Uri selectedImageUri){
+        if (checkPermissionREAD_EXTERNAL_STORAGE(this)) {
+            try {
+                Bitmap srcBmp = BitmapFactory.decodeStream(this.getContentResolver().openInputStream(selectedImageUri), null, null);
+                imageView.setImageBitmap(srcBmp);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    /**
+     * This function check if we have the rights to read the external storage
+     * @param context
+     * @return Boolean
+     */
+    public boolean checkPermissionREAD_EXTERNAL_STORAGE(
+            final Context context) {
+        int currentAPIVersion = Build.VERSION.SDK_INT;
+        if (currentAPIVersion >= android.os.Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(context,
+                    Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        (Activity) context,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    showDialog("External storage", context,
+                            Manifest.permission.READ_EXTERNAL_STORAGE);
+
+                } else {
+                    ActivityCompat
+                            .requestPermissions(
+                                    (Activity) context,
+                                    new String[] { Manifest.permission.READ_EXTERNAL_STORAGE },
+                                    MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                }
+                return false;
+            } else {
+                return true;
+            }
+
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * This function ask the permission to the user to continue
+     * @param msg
+     * @param context
+     * @param permission
+     */
+    public void showDialog(final String msg, final Context context,
+                           final String permission) {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
+        alertBuilder.setCancelable(true);
+        alertBuilder.setTitle("Permission necessary");
+        alertBuilder.setMessage(msg + " permission is necessary");
+        alertBuilder.setPositiveButton(android.R.string.yes,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        ActivityCompat.requestPermissions((Activity) context,
+                                new String[] { permission },
+                                MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                    }
+                });
+        AlertDialog alert = alertBuilder.create();
+        alert.show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // do your stuff
+                } else {
+
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions,
+                        grantResults);
+        }
+    }
 }
