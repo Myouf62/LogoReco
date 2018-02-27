2be41135bf435bd86d6fc82d1368336ef39c05bb
Myouf62 <emilienhenon@gmail.com>
2018-02-27 15:53:59 +0100

    Updated capture management

--- app/src/main/java/com/example/myouf/logoreco/MainActivity.java
+++ app/src/main/java/com/example/myouf/logoreco/MainActivity.java
@@ -14,9 +14,11 @@ import android.graphics.Matrix;
 import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -49,7 +51,9 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, Serializable {
@@ -77,6 +81,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public static Mat[] descriptorsReferencesSprite;
 
     public static SIFT sift;
+
+    private String mCurrentPhotoPath;
 
     /* Partie connexion au serveur */
     //Déclaration de la queue
@@ -254,8 +260,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch(v.getId()){
 
             case R.id.captureButton:
-                Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(takePicture, CAMERA_REQUEST);
+                startCaptureActivity();
                 break;
 
             case R.id.libraryButton:
@@ -332,6 +337,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    protected void startCaptureActivity() {
+        Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePicture.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePicture.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePicture, CAMERA_REQUEST);
+            }
+        }
+    }
+
     protected void startPhotoLibraryActivity(){
         Intent photoLibIntent = new Intent();
         photoLibIntent.setType("image/*");
@@ -346,9 +372,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
         if (requestCode==CAMERA_REQUEST && resultCode==RESULT_OK){
-            selectedImageUri = intent.getData();
-            setImageViewContentFromCamera(selectedImageUri);
+            Bitmap imageBitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, new BitmapFactory.Options());
+            imageViewBase.setImageBitmap(imageBitmap);
+            galleryAddPic();
         }
+    }
+
+    /**
+     * Private method to get a unique file name for a new photo
+     * @return A unique file name for a new photo using a date-time stamp
+     * @throws IOException
+     */
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName, // prefix
+                ".jpg", // suffix
+                storageDir // directory
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    /**
+     * Add the photo to the Media Provider's database, making it available in the Android Gallery application and to other apps
+     */
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
     }
 
     /**
